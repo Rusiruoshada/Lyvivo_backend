a170b587311e6ae19fd8d11ca2ddf5ff9ff3e03c
Rusiruoshada <rusirusamaraweera749@gmail.com>
2024-10-22 22:09:30 +0530

    add postgres connection

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,15 @@ import express from "express";
 // import logger from "morgan";
 import * as path from "path";
 import cors from 'cors';
-import './config/dotenv'
+import './config/dotenv';
+import "reflect-metadata";
 
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
 
 // Routes
-import { index } from "./routes/index";
+import { router } from "./routes/index";
 import mongoose from "mongoose";
+import { AppDataSource } from "./postgres/database";
 // Create Express server
 export const app = express();
 
@@ -17,10 +19,11 @@ const URL = process.env.MONGODB_URL as string
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(URL);
-        console.log('MongoDB Connected Successfully!')
+        const connectMongoDB = await mongoose.connect(URL);
+        const connectPostgreSql = await AppDataSource.initialize();
+        console.log(connectMongoDB && connectPostgreSql && `Databases Connected Successfully!`)
     } catch (error) {
-        console.log('Error with connecting MongoDB', error)
+        console.log('Error with connecting Database', error)
     }
 }
 
@@ -40,7 +43,7 @@ app.use(cors({
 // app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use("/", index);
+app.use("/", router);
 
 app.use(errorNotFoundHandler);
 app.use(errorHandler);
