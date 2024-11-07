8688fdbebf0f8c442388bb338a02d2e847c7f0c3
Rusiruoshada <rusirusamaraweera749@gmail.com>
2024-11-07 23:26:42 +0530

    modify jwt middleware

--- src/middlewares/authenticateJWT.ts
+++ src/middlewares/authenticateJWT.ts
@@ -1,20 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import '../config/dotenv';
+import "../config/dotenv";
 
+export const authenticateJWT = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers["authorization"];
+  console.log("we are in jwt middleware");
+  if (!token) return res.sendStatus(403);
+  const JWT_SECRET_KEY =
+    process.env.JWT_SECRET_KEY || "hello this is SECRET_KEY";
 
-export const authenticateJWT = (req:Request, res:Response, next:NextFunction) => {
-    const token = req.headers['authorization'];
+  jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.body = user; // ask req.user why its not working
 
-    if (!token) return res.sendStatus(403);
-    const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'hello this is SECRET_KEY';
-    
-    jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.body = user; // ask req.user why its not working
-        
-        next();
-    })
-
-
-}
+    next();
+  });
+};
