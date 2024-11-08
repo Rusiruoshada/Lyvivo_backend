ac8ea40b586fc83eadbd119274f44d6a9f08de11
Rusiruoshada <rusirusamaraweera749@gmail.com>
2024-11-08 23:08:23 +0530

    add checkout api

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -151,6 +151,7 @@ export const checkout = async (req: Request, res: Response): Promise<any> => {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: 'usd',
+      automatic_payment_methods: {enabled: true},
     })
     res.send({
       clientSecret: paymentIntent.client_secret,
