e7c99f329b51533d0faf75f7d54ffecd166c4f3e
Rusiruoshada <rusirusamaraweera749@gmail.com>
2025-03-03 15:16:12 +0530

    update authenticate jwt

--- src/middlewares/authenticateJWT.ts
+++ src/middlewares/authenticateJWT.ts
@@ -1,22 +1,32 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import "../config/dotenv";
 
-export const authenticateJWT = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const token = req.headers["authorization"];
-  console.log("we are in jwt middleware");
-  if (!token) return res.sendStatus(403);
-  const JWT_SECRET_KEY =
-    process.env.JWT_SECRET_KEY || "hello this is SECRET_KEY";
+// Middleware to check if the user is an admin
+const authenticateJWT = (req: Request, res: Response, next: any) => {
+  const token = req.headers["authorization"]?.split(" ")[1]; // Extract token from Authorization header
 
-  jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.body = user; // ask req.user why its not working
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const SECRET_KEY = process.env.JWT_SECRET_KEY || "hello this is SECRET_KEY";
+
+  try {
+    // Verify the token and decode it
+    const decoded: any = jwt.verify(token, SECRET_KEY);
+
     
+
+    // Check if the user's role is admin
+    if (decoded.role !== "admin" && decoded.role !== "super_admin") {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+
+    // If user is authorized, move to the next middleware
     next();
-  });
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
 };
+
+export default authenticateJWT;
