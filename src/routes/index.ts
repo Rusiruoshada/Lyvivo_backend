36d988b2ff0719fa89677ace9fb5e581d8fee69c
Rusiruoshada <rusirusamaraweera749@gmail.com>
2025-03-03 15:10:55 +0530

    add new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import * as controller from "../controllers/index";
-import { authenticateJWT } from "../middlewares/authenticateJWT";
+import * as adminAPI from '../controllers/adminAPI/adminAPI';
+import * as signIn from '../controllers/signUp&signIn/signInAPI'
+import * as signUp from "../controllers/signUp&signIn/signUpAPI";
+import * as sendEmail from '../controllers/sendEmailAPI/sendEmailAPI';
+import * as productAPI from '../controllers/productAPI/productAPI'
+import  authenticateJWT  from "../middlewares/authenticateJWT";
 
 export const router = Router();
 
@@ -10,8 +15,16 @@ router.get("/api/homepageProduct", controller.homepageProduct);
 
 router.get("/api/products/:id", controller.productID);
 
-router.post("/api/register", controller.registerUser);
+router.get('/api/getAllProductData', adminAPI.getAllProductData)
 
-router.post("/api/login", controller.login);
+router.post("/api/register", signUp.registerUser);
 
-router.post('/api/checkout', controller.checkout)
+router.post("/api/login", signIn.login);
+
+router.post('/api/checkout', controller.checkout);
+
+router.post('/api/send_email', sendEmail.send_email);
+
+router.post('/api/addNewProduct', adminAPI.addNewProduct)
+
+router.post("/api/productEdit", productAPI.productEdit);
